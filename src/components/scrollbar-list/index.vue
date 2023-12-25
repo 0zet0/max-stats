994b8e3bc7ccebb459c35d48895e1359fd029b22
Timur <begzod200000@gmail.com>
2023-12-25 11:57:25 +0500

    fix

--- src/components/scrollbar-list/index.vue
+++ src/components/scrollbar-list/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="scrollbar-list" ref="scrollcontainer" :style="{ height }">
-    <slot></slot>
+  <div class="scrollbar-list" :style="{ height }">
+    <ion-content style="--background: transparent">
+      <ion-list style="--background: transparent" ref="scrollcontainer">
+        <slot></slot>
+      </ion-list>
+    </ion-content>
   </div>
 </template>
 
 <script setup lang="ts">
+import { IonContent, IonList } from "@ionic/vue";
 import { nextTick, onMounted, ref } from "vue";
 
 defineProps({
@@ -35,9 +40,11 @@ onMounted(() => {
 
 .scrollbar-list {
   position: relative;
-  backface-visibility: hidden;
   width: 100%;
 
-  @include scrollbarY(0);
+  ion-content,
+  ion-list {
+    background: #28333f;
+  }
 }
 </style>
